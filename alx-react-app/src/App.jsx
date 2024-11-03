c6f5c1f7376f584d6ef94482441979cdc415cbf5
Natihan <natnaelyidnekachew@gmail.com>
2024-11-03 13:18:16 +0300

    Add UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -9,6 +10,16 @@ import './App.css'
 
 
 
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <Footer />
+        </div>
+    );
+}
 function App() {
     return (
         <div>
