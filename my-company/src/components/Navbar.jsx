3c19f1c50f0a40881704c9c462e6967440cbf32d
Natihan <natnaelyidnekachew@gmail.com>
2024-11-12 16:42:40 +0300

    update

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ background: '#333', padding: '10px' }}>
+    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
       <ul style={{ listStyleType: 'none', display: 'flex', justifyContent: 'space-around', margin: 0, padding: 0 }}>
         <li><Link to="/" style={linkStyle}>Home</Link></li>
         <li><Link to="/about" style={linkStyle}>About</Link></li>
