59cf09581b648e85db0edcef3fa98a095e88a350
Natihan <natnaelyidnekachew@gmail.com>
2024-11-12 12:19:51 +0300

    UserDetails.jsx

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,18 +1,6 @@
-import React, { useContext } from 'react';
-import UserContext from './UserContext';
 import React from 'react';
 
-function UserDetails() {
-    // Use the useContext hook to access userData from the context
-    const userData = useContext(UserContext);
-  
-    return (
-      <div>
-        <p>Name: {userData.name}</p>
-        <p>Email: {userData.email}</p>
-      </div>
-    );
-  }
+// Create a Context for user data with default values
 const UserContext = React.createContext({
   name: '',
   email: ''
