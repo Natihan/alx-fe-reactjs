9522b53ba3302f7b83324512f8d11824ab073de5
Natnael <natnaelyidnekachew@gmail.com>
2024-12-09 12:44:09 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,12 @@
+// githubService.js
 import axios from 'axios';
 
 export const fetchUserData = async (username) => {
-  const url = `https://api.github.com/users/${username}`;
-  const response = await axios.get(url);
-  return response.data;
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data; // return user data if found
+  } catch (error) {
+    // Throwing an error when the user is not found (e.g., 404 response from GitHub API)
+    throw new Error('User not found');
+  }
 };
